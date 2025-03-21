a47ad95bff53c897a4028d97f236110dd43a62f1
VirtuK <vitorkuiava.vc@gmail.com>
2025-03-21 15:15:27 -0300

    Customizable Board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Item[] items;
+    [SerializeField] private List<Row> rows = new List<Row>();
+    [SerializeField] private GameObject RowPrefab;
 
-    // Update is called once per frame
-    void Update()
+    public int width;
+    public int lenght;
+
+    private void Start()
     {
-        
+        for(int i = 1; i <= width; i++)
+        {
+            GameObject row = Instantiate(RowPrefab, this.transform);
+            Row rowComponent = row.GetComponent<Row>();
+            row.name = "Row_" + i;
+            rows.Add(rowComponent);
+            for (int j = 1; j <= lenght; j++)
+            {
+                rowComponent.CreateTile();
+            }
+        }
     }
 }
--- Assets/Scripts/Row.cs
+++ Assets/Scripts/Row.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Row : MonoBehaviour
 {
-    public Tile[] tiles;
+    [SerializeField] private GameObject tilePrefab;
+    public List<Tile> tiles = new List<Tile>();
+
+    public void CreateTile()
+    {
+        GameObject tile = Instantiate(tilePrefab, this.transform);
+        tiles.Add(tile.GetComponent<Tile>());
+    }
 }
